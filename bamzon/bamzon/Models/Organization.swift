20b7c60944e615bfdbac9ffe4fdf6a012e2abd4b
Brandon <bryan04@calpoly.edu>
2019-01-29 17:22:14 -0800

    changed organization and rsvp

--- bamzon/bamzon/Models/Organization.swift
+++ bamzon/bamzon/Models/Organization.swift
@@ -7,10 +7,54 @@
 //
 
 import Foundation
+import FirebaseDatabase
 
 struct Organization {
     var orgID: ID
     var name: String
     var location: Location
     var teamIDs: [ID]
+    let ref: DatabaseReference?
+    
+    init(orgID: ID, name: String, location: Location, teamIDs: [ID]){
+        self.orgID = orgID
+        self.name = name
+        self.location = location
+        self.teamIDs = teamIDs
+        ref = nil
+    }
+    
+    init(){
+        orgID = ID()
+        name = "N/A"
+        location = Location()
+        teamIDs = []
+        ref = nil
+    }
+    
+    init(key: String, snapshot: DataSnapshot){
+        let snapvalues = snapshot.value as! [String : AnyObject]
+        orgID = snapvalues["orgID"] as? ID ?? ID()
+        name = snapvalues["name"] as? String ?? "N/A"
+        location = snapvalues["location"] as? Location ?? Location()
+        
+        for teamID in (snapshot.childSnapshot(forPath:
+            "teamIDs").children.allObjects as! [DataSnapshot]){
+                teamIDs.append(ID(key: teamID.key, snapshot: teamID))
+        }
+        
+        ref = snapshot.ref
+
+    }
+    
+    func toAnyObject() -> Any {
+        return [
+            "orgID" : orgID,
+            "name" : name,
+            "location" : location,
+            "teamIDS" : teamIDs
+        ]
+    }
+    
+    
 }
--- bamzon/bamzon/Models/RSVP.swift
+++ bamzon/bamzon/Models/RSVP.swift
@@ -7,9 +7,44 @@
 //
 
 import Foundation
+import FirebaseDatabase
 
 struct RSVP {
     var yesUserIDs: [ID]?
     var noUserIDs: [ID]?
     var pendingUserIDs: [ID]?
+    let ref: DatabaseReference?
+    
+    init(yesUserIDs: [ID]?, noUserIDs: [ID]?, pendingUserIDs: [ID]?){
+        self.yesUserIDs = yesUserIDs
+        self.noUserIDs = noUserIDs
+        self.pendingUserIDs = pendingUserIDs
+        ref = nil
+    }
+    
+    init(){
+        ref = nil
+    }
+    
+    init(key: String, snapshot: DataSnapshot){
+        let snapvalues = snapshot.value as! [String : AnyObject]
+        
+        for yesUserID in (snapshot.childSnapshot(forPath:
+            "yesUserIDs").children.allObjects as! [DataSnapshot]) {
+                yesUserIDs!.append(ID(key: yesUserID.key, snapshot: yesUserID))
+        }
+        
+        for noUserID in (snapshot.childSnapshot(forPath:
+            "noUserIDs").children.allObjects as! [DataSnapshot]) {
+                noUserIDs!.append(ID(key: noUserID.key, snapshot: noUserID))
+        }
+        
+        for pendingUserID in (snapshot.childSnapshot(forPath:
+            "pendingUserIDs").children.allObjects as! [DataSnapshot]) {
+                pendingUserIDs!.append(ID(key: pendingUserID.key, snapshot: pendingUserID))
+        }
+        
+    }
+    
+    
 }
