464c84b35c6ab2244161c41b5a2acfa25b73faf4
Kellie Banzon <kelliebanzon@gmail.com>
2019-02-07 17:14:24 -0800

    fixed template file push & added my stupid pod nonsenses

---
+++ FileTemplates/Custom/VC.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,27 @@
+//___FILEHEADER___
+
+import Foundation
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController, DisplayableProtocol {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        display()
+    }
+    
+    func display() {
+        // TODO: display
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
