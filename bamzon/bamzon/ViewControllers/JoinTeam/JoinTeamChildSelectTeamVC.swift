aa09924d0fd71ef06af50b310e6eb346dfb4aa90
Kellie Banzon <kelliebanzon@gmail.com>
2019-02-21 20:14:11 -0800

    JoinTeam container views hierarchy

---
+++ bamzon/bamzon/ViewControllers/JoinTeam/JoinTeamChildSelectTeamVC.swift
@@ -0,0 +1,44 @@
+//
+//  JoinTeamChildSelectTeamVC.swift
+//  bamzon
+//
+//  Created by Kellie Banzon on 02/21/19.
+//  Copyright © 2019 bamzon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class JoinTeamChildSelectTeamVC: UIViewController, DisplayableProtocol {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        display()
+    }
+    
+    func display() {
+        // TODO: display
+        view.backgroundColor = UIColor(named: "TSTeal")
+
+        // TODO: temporary identifier code. delete this once you write the real display func
+        // Temp Label
+        let tempLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 120))
+        tempLabel.center = CGPoint(x: view.frame.midX, y: view.frame.midY)
+        tempLabel.font = UIFont(name: "HelveticaNeue-Medium", size: 20)
+        tempLabel.textAlignment = .center
+        tempLabel.numberOfLines = 1
+        tempLabel.textColor = .white
+        tempLabel.text = "JoinTeamChildSelectTeamVC"
+        self.view.addSubview(tempLabel)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
---
+++ bamzon/bamzon/ViewControllers/JoinTeam/JoinTeamParentVC.swift
@@ -0,0 +1,57 @@
+//
+//  JoinTeamVC.swift
+//  bamzon
+//
+//  Created by Krein, Kevin on 12/4/18.
+//  Copyright © 2018 bamzon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class JoinTeamParentVC: UIViewController, DisplayableProtocol {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        display()
+    }
+    
+    func display() {
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(containerView)
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            containerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+            ])
+
+        // add child view controller view to container
+        let firstChildVC = storyboard!.instantiateViewController(withIdentifier: "JoinTeamChildSelectTeamVC")
+        addChildViewController(firstChildVC)
+        firstChildVC.view.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(firstChildVC.view)
+
+        NSLayoutConstraint.activate([
+            firstChildVC.view.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            firstChildVC.view.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            firstChildVC.view.topAnchor.constraint(equalTo: containerView.topAnchor),
+            firstChildVC.view.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+            ])
+
+        firstChildVC.didMove(toParentViewController: self)
+    }
+    
+    func selectOrganization(org: Organization) {
+        // TODO: implement select organization
+    }
+    
+    func selectTeam(teamID: CLong) {
+        // TODO: implement select team
+    }
+    
+    func sendJoinRequest(req: JoinRequest) {
+        // TODO: implement send join request
+    }
+}
