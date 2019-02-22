92c9863406c990e099d7efece4b83a91d9378efe
Kevin Krein <kevin.krein98@gmail.com>
2019-02-21 16:07:31 -0800

    Comment out database write
    also uncommented kellies nav bar thing after accidentially commenting it out last time

--- bamzon/bamzon/ViewModels/CreateTeamVM.swift
+++ bamzon/bamzon/ViewModels/CreateTeamVM.swift
@@ -12,9 +12,10 @@ class CreateTeamVM {
     static func createTeam(teamName: String, orgName: String, sport: String) {
         let teamID = IDUtility.generateTeamID()
         //TODO: Check if the org name exists already. not sure where we wanna do this but it should be done somewhere and someow.
+        //TODO: comment out write to database
         let orgID = IDUtility.generateOrgID()
         
         let team = Team.init(teamID: teamID, orgID: orgID, userIDs: nil, teamName: teamName, sport: sport, stats: nil, calendar: nil, joinReqIDs: nil, blacklistUserIDs: nil)
-        DBUtility.writeToDB(objToWrite: team)
+        //DBUtility.writeToDB(objToWrite: team)
     }
 }
