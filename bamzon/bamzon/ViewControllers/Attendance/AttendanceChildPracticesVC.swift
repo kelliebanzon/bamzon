344a7abb80a9b5496c7c2c15cf84b55463712a52
Kyle Vu <kovu@calpoly.edu>
2019-03-13 16:27:51 -0700

    Initial Practices VC - Part 1

--- bamzon/bamzon/ViewControllers/Attendance/AttendanceChildPracticesVC.swift
+++ bamzon/bamzon/ViewControllers/Attendance/AttendanceChildPracticesVC.swift
@@ -10,10 +10,19 @@ import Foundation
 import UIKit
 import XLPagerTabStrip
 
-class AttendanceChildPracticesVC: UIViewController, IndicatorInfoProvider, DisplayableProtocol, RefreshableProtocol, EditableProtocol {
+class AttendanceChildPracticesVC: UIViewController, UITableViewDelegate, UITableViewDataSource, IndicatorInfoProvider, DisplayableProtocol, RefreshableProtocol, EditableProtocol {
 
-    var tempLabel = UILabel()
-
+    let cellId = "cellId"
+    
+    var datesTableView: UITableView = UITableView()
+    var datesList: [String] = ["03/01/2019", "03/02/2019", "03/03/2019", "03/04/2019", "03/05/2019", "03/06/2019", "03/07/2019", "03/08/2019", "03/09/2019", "03/10/2019", "03/11/2019", "03/12/2019", "03/13/2019", "03/14/2019 - Pi Day!"]
+    
+    let offsets: [String: CGFloat] = [
+        "vertEdges": 40,
+        "horizEdges": 20,
+        "inTableSpacing": 12
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         display()
@@ -21,24 +30,50 @@ class AttendanceChildPracticesVC: UIViewController, IndicatorInfoProvider, Displ
 
     func display() {
         // TODO: implement display
-        view.backgroundColor = UIColor(named: "TSOrange")
-
-        tempLabel = createDefaultLabel(text: description, numLines: 0)
-        tempLabel.textAlignment = .center
-        tempLabel.lineBreakMode = .byWordWrapping
-        self.view.addSubview(tempLabel)
-
+        view.backgroundColor = UIColor(named: "TSTeal")
+        datesTableView.backgroundColor = UIColor(named: "TSTeal")
+        datesTableView.dataSource = self
+        datesTableView.delegate = self
+        datesTableView.register(AttendancePracticesTableViewCell.self, forCellReuseIdentifier: "cellId")
+        self.view.addSubview(datesTableView)
         setupAutoLayout()
     }
 
     func setupAutoLayout() {
         let margins = view.safeAreaLayoutGuide
-
-        tempLabel.translatesAutoresizingMaskIntoConstraints = false
-        let tempLabelVertConstraint = tempLabel.centerYAnchor.constraint(equalTo: margins.centerYAnchor)
-        let tempLabelLeadingConstraint = tempLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 20)
-        let tempLabelTrailingConstraint = margins.trailingAnchor.constraint(equalTo: tempLabel.trailingAnchor, constant: 20)
-        self.view.addConstraints([tempLabelVertConstraint, tempLabelLeadingConstraint, tempLabelTrailingConstraint])
+        datesTableView.translatesAutoresizingMaskIntoConstraints = false
+        let tableTop = datesTableView.topAnchor.constraint(equalTo: view.topAnchor)
+        let tableLeading = datesTableView.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let tableTrailing = margins.trailingAnchor.constraint(equalTo: datesTableView.trailingAnchor)
+        let tableBottom = margins.bottomAnchor.constraint(equalTo: datesTableView.bottomAnchor, constant: 85)
+        self.view.addConstraints([tableTop, tableLeading, tableTrailing, tableBottom])
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Number of rows to present
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datesList.count
+    }
+    
+    // Populate value at selected row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // swiftlint:disable force_cast
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! AttendancePracticesTableViewCell
+        // swiftlint:enable force_cast
+        cell.practiceDate.text = datesList[indexPath.row]
+        cell.practiceDate.textColor = .white
+        cell.backgroundColor = UIColor(named: "TSTeal")
+        return cell
+    }
+    
+    // Function to handle what setting to call
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        alert(withTitle: "Date Selected", withMessage: "Show " + datesList[indexPath.row] + "'s attendance")
+        let selectedCell = tableView.cellForRow(at: indexPath)
+        selectedCell?.contentView.backgroundColor = UIColor(named: "TSYellow")
     }
 
     func refresh() {
---
+++ bamzon/bamzon/ViewControllers/Attendance/AttendancePracticesTableViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  AttendancePracticesTableViewCell.swift
+//  bamzon
+//
+//  Created by Kyle Vu on 3/13/19.
+//  Copyright © 2019 bamzon. All rights reserved.
+//
+
+import UIKit
+
+class AttendancePracticesTableViewCell: UITableViewCell {
+
+    var practiceDate = UILabel()
+    var attendancePercentage = UILabel()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = UIColor(named: "TSTeal")
+        
+        practiceDate = createLabelToConstrain(withText: "mm/dd/yyyy", alignment: .left, boldType: "", fontSize: 20, numLines: 1, hasScaleFactor: true)
+        
+        addSubviews()
+        setupAutoLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
+        contentView.addSubview(practiceDate)
+    }
+    
+    func setupAutoLayout() {
+        //        //image constraints
+        //        let imgLeftConstraint = imgUser.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 10)
+        //        let imgVertConstraint = imgUser.topAnchor.constraint(equalTo: self.contentView.centerYAnchor, constant: -35)
+        //        let imgWidthConstraint = imgUser.widthAnchor.constraint(equalToConstant: 70)
+        //        let imgHeightConstraint = imgUser.heightAnchor.constraint(equalToConstant: 70)
+        //        self.contentView.addConstraints([imgLeftConstraint, imgVertConstraint, imgWidthConstraint, imgHeightConstraint])
+        
+        // Date Label constraints
+        let dateLeftConstraint = practiceDate.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 10)
+        let dateTopConstraint = practiceDate.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 10)
+        let dateWidthConstraint = practiceDate.leadingAnchor.constraint(lessThanOrEqualTo: self.contentView.centerXAnchor)
+        self.contentView.addConstraints([dateLeftConstraint, dateTopConstraint, dateWidthConstraint])
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+
+}
