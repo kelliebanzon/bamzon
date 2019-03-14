7a46b7387a6ea4f6af345ce52a294f2f77875a48
Kellie Banzon <kelliebanzon@gmail.com>
2019-03-14 13:48:19 -0700

    added close to final create account screen

--- bamzon/bamzon/ViewControllers/CreateAccount/CreateAccountChildSuccessVC.swift
+++ bamzon/bamzon/ViewControllers/CreateAccount/CreateAccountChildSuccessVC.swift
@@ -12,6 +12,7 @@ class CreateAccountChildSuccessVC: UIViewController, DisplayableProtocol {
 
     var name: String?
     var successLabel = UILabel()
+    var closeButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +22,10 @@ class CreateAccountChildSuccessVC: UIViewController, DisplayableProtocol {
     func display() {
         view.backgroundColor = UIColor(named: "TSTeal")
 
-        successLabel = createDefaultHeader2Label(text: createGreetingMessage(name: name), numLines: 0)
+        closeButton = createCloseButton(action: #selector(goToLogin))
+        self.view.addSubview(closeButton)
+
+        successLabel = createDefaultHeader2Label(text: createGreetingMessage(name: name), numLines: 0, fontAlignment: .center)
         successLabel.lineBreakMode = .byWordWrapping
         self.view.addSubview(successLabel)
 
@@ -31,12 +35,18 @@ class CreateAccountChildSuccessVC: UIViewController, DisplayableProtocol {
     func setupAutoLayout() {
         let margins = view.safeAreaLayoutGuide
 
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        let closeTrailing = margins.trailingAnchor.constraint(equalTo: closeButton.trailingAnchor, constant: 30)
+        let closeTop = closeButton.topAnchor.constraint(equalTo: margins.topAnchor, constant: 30)
+        let closeHeight = closeButton.heightAnchor.constraint(equalToConstant: 24)
+        let closeAspect = closeButton.widthAnchor.constraint(equalTo: closeButton.heightAnchor)
+        self.view.addConstraints([closeTrailing, closeTop, closeHeight, closeAspect])
+
         successLabel.translatesAutoresizingMaskIntoConstraints = false
         let successVert = successLabel.centerYAnchor.constraint(equalTo: margins.centerYAnchor)
         let successLeading = successLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 20)
         let successTrailing = successLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: -20)
         self.view.addConstraints([successVert, successLeading, successTrailing])
-
     }
 
     func createGreetingMessage(name: String?) -> String {
@@ -47,6 +57,11 @@ class CreateAccountChildSuccessVC: UIViewController, DisplayableProtocol {
             final += ", " + name
         }
         return final + end
+    }
+
+    @objc func goToLogin(sender: UIButton) {
+        print("goToLogin")
+        self.setRootView(toVC: "LoginVC")
     }
 
 }
